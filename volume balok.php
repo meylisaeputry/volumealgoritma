425008d07143fee0cc04bb1e85aa53b0fafc63b7
meylisaeputry <putrymeylisa315@gmail.com>
2024-08-12 20:54:26 +0700

    menambah volume balok

---
+++ volume balok.php
@@ -0,0 +1,17 @@
+<?php
+//Mulai
+//Deklarasi,panjang,lebar,dan tinggi
+$panjang =5;
+$lebar =4;
+$tinggi =3;
+//Tentukan nilai panjang
+//Tentukan nilai lebar
+//Tentukan nilai tinggi
+echo "panjang=". $panjang;
+echo "<br>lebar=". $lebar;
+echo "<br>tinggi=". $tinggi;
+//Hitung volume dengan panjang dikali lebar dikali tinggi
+$volume=$panjang*$lebar*$tinggi;
+//Tampilkan hasil perkalian
+echo "<br>hasil volume balok adalah=". $volume;
+//Selesai

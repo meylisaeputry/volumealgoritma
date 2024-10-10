ae41fb7725baa892297114e5642818bff87e4d10
meylisaeputry <putrymeylisa315@gmail.com>
2024-10-10 12:46:20 +0700

    Menambah form

---
+++ 03 output perulangan nama .php
@@ -0,0 +1,30 @@
+
+<?php
+//Mulai
+//Iniliasisasi variabel i dengan nilai 1.
+//Lakukan perulangan selama i kurang dari atau sama dengan 100:
+echo "<h2>Selamat anda berhasil login</h2>";
+for ($i = 1; $i <= 10; $i++)
+//Cetak nilai i.
+{
+echo $i . "nama saya Meylisa eka putry<br>";
+//Akhiri perulangan
+}
+
+//Selesai
+?>
+   
+<p>
+<a href="02 index menentukan hari libur .php">
+      <button>Kembali ke Input</button>
+</a>
+</p>
+
+<p>atau kembali ke : </p>
+<a href="http://localhost/">
+      <button>Localhost</button>
+</a>
+<a href="../">
+      <button>Folder Awal</button>
+</a>
+<br><br><strong>By meylisa eka putry</strong=";
